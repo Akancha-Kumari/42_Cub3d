ce16c5e5fcc203cfe6a4a1bb97d23f3bfab2ff27
Akancha Kumari <akxa75041@gmail.com>
2025-10-02 17:24:03 +0300

    changed error page

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: akumari <akumari@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 14:38:08 by akumari           #+#    #+#             */
-/*   Updated: 2025/10/02 16:48:10 by akumari          ###   ########.fr       */
+/*   Updated: 2025/10/02 17:14:22 by akumari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,22 @@ void	error_exit(char *error_msg)
 void	line_exit(t_arena *arena, char *line, char *error_msg)
 {
 	free(line);
-	arena_destroy(arena);
+	if (arena)
+		arena_destroy(arena);
 	error_exit(error_msg);
 }
 
 void	arena_exit(t_arena *arena, char *error_msg)
 {
-	arena_destroy(arena);
+	if (arena)
+		arena_destroy(arena);
 	error_exit(error_msg);
 }
 
+// Main cleanup function
 void	cleanup_game(t_game *game)
 {
+	game->map = NULL;
 	if (game->tex_north)
 		mlx_delete_texture(game->tex_north);
 	if (game->tex_south)
@@ -47,3 +51,19 @@ void	cleanup_game(t_game *game)
 	if (game->mlx)
 		mlx_terminate(game->mlx);
 }
+
+// void	cleanup_game(t_game *game)
+// {
+// 	if (game->tex_north)
+// 		mlx_delete_texture(game->tex_north);
+// 	if (game->tex_south)
+// 		mlx_delete_texture(game->tex_south);
+// 	if (game->tex_east)
+// 		mlx_delete_texture(game->tex_east);
+// 	if (game->tex_west)
+// 		mlx_delete_texture(game->tex_west);
+// 	if (game->img)
+// 		mlx_delete_image(game->mlx, game->img);
+// 	if (game->mlx)
+// 		mlx_terminate(game->mlx);
+// }
